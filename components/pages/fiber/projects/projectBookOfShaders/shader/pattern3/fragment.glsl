9f799328c4ffdbca69fe9f7337a78ca9b7676c9a
Younsoo Tom Lim <tomandlim@gmail.com>
2023-02-13 22:39:04 +0900

    Feat: Add first algorithmic drawing

--- components/pages/fiber/projects/projectBookOfShaders/shader/pattern3/fragment.glsl
+++ components/pages/fiber/projects/projectBookOfShaders/shader/pattern3/fragment.glsl
@@ -6,14 +6,17 @@ float random (vec2 st) {
         43758.5453123);
 }
 
+// Plot a line on Y using a value between 0.0-1.0
+float plot(vec2 strength) {    
+    return smoothstep(0.01, 0.0, abs(strength.y - strength.x));
+}
+
 void main() {
-    vec2 strength = vUv;
+    float strength = vUv.x;
+    vec3 color = vec3(strength);
 
-    strength *= 10.; // Scale the coordinate system by 10
-    vec2 ipos = floor(strength);  // get the integer coords
-
-    // Assign a random value based on the integer coord
-    vec3 color = vec3(random( ipos ));
+    float pct = plot(vUv);
+    color = (1.0-pct) * color + pct * vec3(0.0,1.0,0.0);
 
     gl_FragColor = vec4(color,1.0);
 }
--- components/pages/fiber/projects/projectBookOfShaders/shader/shaderMap.ts
+++ components/pages/fiber/projects/projectBookOfShaders/shader/shaderMap.ts
@@ -2,10 +2,13 @@ import Pattern1Fragment from "raw-loader!glslify-loader!./pattern1/fragment.glsl
 import Pattern1Vertex from "raw-loader!glslify-loader!./pattern1/vertex.glsl";
 import Pattern2Fragment from "raw-loader!glslify-loader!./pattern2/fragment.glsl";
 import Pattern2Vertex from "raw-loader!glslify-loader!./pattern2/vertex.glsl";
+import Pattern3Fragment from "raw-loader!glslify-loader!./pattern3/fragment.glsl";
+import Pattern3Vertex from "raw-loader!glslify-loader!./pattern3/vertex.glsl";
 
 export const shaderMap: ShaderMap[] = [
   { fragment: Pattern1Fragment, vertex: Pattern1Vertex, name: '2D random'},
   { fragment: Pattern2Fragment, vertex: Pattern2Vertex, name: 'Mosaic' },
+  { fragment: Pattern3Fragment, vertex: Pattern3Vertex, name: 'Algorithmic drawing 1'},
 ];
 
 export interface ShaderMap {
