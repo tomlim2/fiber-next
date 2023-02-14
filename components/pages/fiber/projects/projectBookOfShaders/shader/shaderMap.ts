4e3dc361291f03a9648f880293b363ef25e1e814
Younsoo Tom Lim <tomandlim@gmail.com>
2023-02-14 21:07:14 +0900

    Feat: Add drawing for pow, step, and smoothstep

--- components/pages/fiber/projects/projectBookOfShaders/shader/shaderMap.ts
+++ components/pages/fiber/projects/projectBookOfShaders/shader/shaderMap.ts
@@ -4,11 +4,20 @@ import Pattern2Fragment from "raw-loader!glslify-loader!./pattern2/fragment.glsl
 import Pattern2Vertex from "raw-loader!glslify-loader!./pattern2/vertex.glsl";
 import Pattern3Fragment from "raw-loader!glslify-loader!./pattern3/fragment.glsl";
 import Pattern3Vertex from "raw-loader!glslify-loader!./pattern3/vertex.glsl";
+import Pattern4Fragment from "raw-loader!glslify-loader!./pattern4/fragment.glsl";
+import Pattern4Vertex from "raw-loader!glslify-loader!./pattern4/vertex.glsl";
+import Pattern5Fragment from "raw-loader!glslify-loader!./pattern5/fragment.glsl";
+import Pattern5Vertex from "raw-loader!glslify-loader!./pattern5/vertex.glsl";
+import Pattern6Fragment from "raw-loader!glslify-loader!./pattern6/fragment.glsl";
+import Pattern6Vertex from "raw-loader!glslify-loader!./pattern6/vertex.glsl";
 
 export const shaderMap: ShaderMap[] = [
   { fragment: Pattern1Fragment, vertex: Pattern1Vertex, name: '2D random'},
   { fragment: Pattern2Fragment, vertex: Pattern2Vertex, name: 'Mosaic' },
-  { fragment: Pattern3Fragment, vertex: Pattern3Vertex, name: 'Algorithmic drawing 1'},
+  { fragment: Pattern3Fragment, vertex: Pattern3Vertex, name: 'Algorithmic drawing UV'},
+  { fragment: Pattern4Fragment, vertex: Pattern4Vertex, name: 'Algorithmic drawing Pow'},
+  { fragment: Pattern5Fragment, vertex: Pattern5Vertex, name: 'Algorithmic drawing Step'},
+  { fragment: Pattern6Fragment, vertex: Pattern6Vertex, name: 'Algorithmic drawing Smoothstep'},
 ];
 
 export interface ShaderMap {
