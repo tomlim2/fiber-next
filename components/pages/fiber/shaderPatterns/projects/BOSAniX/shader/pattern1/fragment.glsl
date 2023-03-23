3df0d5bd7ca6d2817c846822971123c6a2583a23
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-03-23 20:41:31 +0900

    Feat: Add explore patterns

--- components/pages/fiber/shaderPatterns/projects/BOSAniX/shader/pattern1/fragment.glsl
+++ components/pages/fiber/shaderPatterns/projects/BOSAniX/shader/pattern1/fragment.glsl
@@ -11,16 +11,27 @@ float random(in vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
+float pattern(vec2 st, vec2 v, float t) {
+    vec2 p = floor(st + v);
+    return step(t, random(100. + p * .000001) + random(p.y) * 0.5);
+}
+
 void main() {
     vec2 st = vUv;
-    vec3 color = vec3(0.);
     //scale
-    vec2 grid = vec2(10.,2.0);
+    vec2 grid = vec2(10., 20.0);
     st *= grid;
 
     vec2 ipos = floor(st);
+    vec2 fpos = fract(st);
 
-    color = vec3(random(ipos));
+    vec2 vel = vec2(uTime * 2. * max(grid.x, grid.y)); // time
+
+    vel *= vec2(.0, -1.) * random(1.0 + ipos.x); // direction
+    vec3 color = vec3(0.);
+
+    color = vec3(pattern(st, vel, 0.5));
+    color *= step(0., fpos.x);
 
     gl_FragColor = vec4(color, 1.0);
 }
