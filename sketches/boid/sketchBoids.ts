ab72612f527172042f68184a870893183d8f857e
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-05-23 21:51:26 +0900

    Feat: Add single boid

--- sketches/boid/sketchBoids.ts
+++ sketches/boid/sketchBoids.ts
@@ -1,11 +1,19 @@
 import p5 from "p5";
+import Boid from "./boid";
+
+const flock: Boid[] = [];
 
 export default function sketchBoids(p: p5) {
   p.setup = () => {
     p.createCanvas(600, 600);
+    flock.push(new Boid(p));
   };
 
   p.draw = () => {
     p.background(51);
+
+    for (let boid of flock) {
+      boid.show(p);
+    }
   };
 }
