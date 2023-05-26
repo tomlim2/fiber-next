4cc6a41f82fe20ce970ac4e07424e80236de292d
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-05-26 20:37:51 +0900

    Feat: Add rotation

--- components/sections/fiber/shaderPatterns/projects/BOSAniBM/shader/pattern1/fragment.glsl
+++ components/sections/fiber/shaderPatterns/projects/BOSAniBM/shader/pattern1/fragment.glsl
@@ -17,15 +17,25 @@ uniform vec3 uColorA;
 uniform vec3 uColorB;
 
 varying vec2 vUv;
+mat2 rotate2d(float angle) {
+    return mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+}
+
+vec2 boxi(in vec2 _st, in vec2 _size) {
+    _size = vec2(.5) - _size * .5;
+    vec2 uv = smoothstep(_size, _size + vec2(.001), _st);
+    uv *= smoothstep(_size, _size + vec2(.001), vec2(1.) - _st);
+    return vec2(uv.x, uv.y);
+}
 
 void main() {
     vec2 st = vUv;
-    vec2 boxSize = vec2(.5);
-    boxSize = vec2(.5) - boxSize * .5;
+    vec2 f_st = fract(st);
+    f_st -= vec2(0.5);
+    vec2 c1 = f_st * rotate2d(PI * uTime / 2.);
+    c1 += vec2(0.5);
 
-    vec2 simpleBox = smoothstep(boxSize, boxSize + vec2(.001), st);
-    simpleBox *= smoothstep(boxSize, boxSize + vec2(0.001), vec2(1.0) - st);
-    vec3 color = vec3(simpleBox.x * simpleBox.y);
+    vec3 color = vec3(boxi(c1, vec2(.75, .1)), 1.0);
 
     gl_FragColor = vec4(color, 1.0);
 }
