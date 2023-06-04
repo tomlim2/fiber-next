98ce75ac4ae46d8e946497756c7e3b0fa9af6eb2
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-06-04 23:40:53 +0900

    Fix: Redirect issue

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 
-import SectionAbout from "@/components/sections/about/sectionAbout";
+import SectionAbout from "../../components/sections/about/SectionAbout";
 
 export const metadata: Metadata = {
-    title: "About | yslim",
-    description: "Note about geometry node",
-  };
+  title: "About | yslim",
+  description: "Note about geometry node",
+};
 
 const About = () => {
   return (
