0edd45a8a4b85a57eafffa0fc6352f92406a2e41
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-06-12 21:11:04 +0900

    Feat: Add load models

--- components/sections/fiber/journey-models/Experience.tsx
+++ components/sections/fiber/journey-models/Experience.tsx
@@ -3,18 +3,25 @@ import {
   Stage,
   useHelper,
   OrbitControls,
+  ContactShadows,
 } from "@react-three/drei";
 import { Suspense, useRef } from "react";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
 import { useControls } from "leva";
 import Model from "./Halmet";
-import DreiModel from "./Hamburger";
+import Hamburger from "./Hamburger";
+import Fox from "./Fox";
 
 const Experience = () => {
   const cube = useRef() as any;
   const directionalLight = useRef() as any;
   useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+  const { color, opacity, blur } = useControls("contact shadows", {
+    color: "#1d8f75",
+    opacity: { value: 0.4, min: 0, max: 1 },
+    blur: { value: 2.8, min: 0, max: 10 },
+  });
 
   return (
     <>
@@ -22,8 +29,24 @@ const Experience = () => {
 
       <OrbitControls makeDefault />
 
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={1.5}
+        shadow-normalBias={0.04}
+      />
       <ambientLight intensity={0.5} />
+
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={512}
+        far={5}
+        color={color}
+        opacity={opacity}
+        blur={blur}
+        frames={1}
+      />
 
       <Suspense
         fallback={
@@ -33,8 +56,11 @@ const Experience = () => {
           </mesh>
         }
       >
-        <DreiModel />
+        {/* <Hamburger scale={0.35} /> */}
+        <Fox />
       </Suspense>
+
+      
 
       <mesh
         receiveShadow
--- components/sections/fiber/journey-models/Hamburger.tsx
+++ components/sections/fiber/journey-models/Hamburger.tsx
@@ -1,8 +1,39 @@
 import { useGLTF } from "@react-three/drei";
 
-export default function Hamburger() {
-  const model = useGLTF("/assets/models/hamburger.glb");
-  return <primitive object={model.scene} scale={0.35} />;
+export default function Hamburger(props: any) {
+  const { nodes, materials } = useGLTF("/assets/models/hamburger.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.bottomBun.geometry}
+        material={materials.BunMaterial}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.meat.geometry}
+        material={materials.SteakMaterial}
+        position={[0, 2.82, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.cheese.geometry}
+        material={materials.CheeseMaterial}
+        position={[0, 3.04, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.topBun.geometry}
+        material={materials.BunMaterial}
+        // position={[0, 1.77, 0]}
+        position={[0, 3.77, 0]}
+      />
+    </group>
+  );
 }
 
 useGLTF.preload("/assets/models/hamburger.glb");
