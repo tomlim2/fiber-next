93798ad3a494de80b2bf646a129839e61a05a0b8
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-06-25 15:23:49 +0900

    Feat: Update router in home

--- components/sections/home/SectionHome.tsx
+++ components/sections/home/SectionHome.tsx
@@ -1,74 +1,84 @@
 import styles from "../../../app/page.module.scss";
 import Link from "next/link";
 const SectionHome = () => {
+  const routeInfo = [
+    {
+      routeCategoryName: "Fiber",
+      routeList: [
+        { routeName: "Drei-Portal", routeHref: "/fiber/drei-portal" },
+        { routeName: "Vertex-Fragnent", routeHref: "/fiber/vertex-fragnent" },
+        { routeName: "Fragnent-Time", routeHref: "/fiber/fragnent-time" },
+        { routeName: "Fragnent-Still", routeHref: "/fiber/fragnent-still" },
+        {
+          routeName: "Journey-Physics-Base",
+          routeHref: "/fiber/journey-physics",
+        },
+        {
+          routeName: "Journey-Physics-GLTF",
+          routeHref: "/fiber/journey-physics-gltf",
+        },
+        {
+          routeName: "Journey-Postprocess",
+          routeHref: "/fiber/journey-postprocess",
+        },
+        {
+          routeName: "Journey-Models",
+          routeHref: "/fiber/journey-models",
+        },
+        {
+          routeName: "Journey-Stage",
+          routeHref: "/fiber/journey-stage",
+        },
+        {
+          routeName: "Journey-Env",
+          routeHref: "/fiber/journey-env",
+        },
+      ],
+    },
+    {
+      routeCategoryName: "P5",
+      routeList: [
+        { routeName: "Boid", routeHref: "/p5/boid" },
+        { routeName: "Vector Field", routeHref: "/p5/vfa" },
+      ],
+    },
+    {
+      routeCategoryName: "Blender",
+      routeList: [
+        { routeName: "Geometry Node", routeHref: "/blender/geometry-node" },
+      ],
+    },
+    {
+      routeCategoryName: "Design",
+      routeList: [{ routeName: "UI", routeHref: "/design/ui" }],
+    },
+  ];
   return (
     <section>
       <ul className={styles.works}>
-        <li className="shader">
-          <h3 className="category-name">FIBER</h3>
-          <ul>
-          <li>
-              <Link href="/fiber/drei-portal">Drei-Portal</Link>
+        {routeInfo.map((categoryItem, categoryIndex) => {
+          return (
+            <li className="blender" key={categoryIndex}>
+              <h3 className="category-name">
+                {categoryItem.routeCategoryName}
+              </h3>
+              <ul>
+                {categoryItem.routeList.map((routeItem, routeIndex) => {
+                  return (
+                    <li key={routeIndex}>
+                      <Link href={routeItem.routeHref}>
+                        {routeItem.routeName}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </li>
-            <li>
-              <Link href="/fiber/vertex-fragnent">Vertex-Fragnent</Link>
-            </li>
-            <li>
-              <Link href="/fiber/fragnent-time">Fragnent-Time</Link>
-            </li>
-            <li>
-              <Link href="/fiber/fragnent-still">Fragnent-Still</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-physics">Journey-Physics-Base</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-physics-gltf">Journey-Physics-GLTF</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-postprocess">Journey-Postprocess</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-models">Journey-Models</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-stage">Journey-Stage</Link>
-            </li>
-            <li>
-              <Link href="/fiber/journey-env">Journey-Env</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="p5">
-          <h3 className="category-name">P5</h3>
-          <ul>
-            <li>
-              <Link href="/p5/boid">Boids</Link>
-            </li>
-            <li>
-              <Link href="/p5/vfa">Vector Field</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="blender">
-          <h3 className="category-name">BLENDER</h3>
-          <ul>
-            <li>
-              <Link href="/blender/geometry-node">Geometry Node</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="design">
-          <h3 className="category-name">DESIGN</h3>
-          <ul>
-            <li>
-              <Link href="/design/ui">UI</Link>
-            </li>
-          </ul>
-        </li>
+          );
+        })}
       </ul>
     </section>
   );
 };
 
-export default SectionHome
+export default SectionHome;
