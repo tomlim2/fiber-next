5e1a6e9fbb68b6110205d5811792dfaaf389651d
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-07-21 21:05:02 +0900

    Revision about writing

--- components/sections/about/SectionAbout.tsx
+++ components/sections/about/SectionAbout.tsx
@@ -3,7 +3,7 @@ import styles from "./SectionAbout.module.scss";
 const SectionAbout = () => {
   return (
     <div className={styles["section-about"]}>
-      <p>This website is only for collecting that I studied in digital based media thesedays.</p>
+      <p>The purpose of this website is only for collecting that I studied in digital based media thesedays.</p>
     </div>
   );
 };
