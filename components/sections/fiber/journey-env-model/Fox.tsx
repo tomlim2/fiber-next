e77bec6618d4671a26d210a9b88a2d2c40f6905e
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-07-25 21:12:23 +0900

    Update fox

--- components/sections/fiber/journey-env-model/Fox.tsx
+++ components/sections/fiber/journey-env-model/Fox.tsx
@@ -2,12 +2,14 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 import { useEffect } from "react";
 import { useControls } from "leva";
 export default function Fox() {
-  const fox = useGLTF("/assets/models/glb_cube_01.glb");
+  const fox = useGLTF("/assets/models/Fox/glTF/Fox.gltf");
   const animations = useAnimations(fox.animations, fox.scene) as any;
   const { animationName } = useControls({
     animationName: { options: animations.names },
   });
   useEffect(() => {
+    console.log(fox);
+    
     const action = animations.actions[animationName as any];
     action.reset().fadeIn(0.5).play();
 
@@ -19,7 +21,7 @@ export default function Fox() {
   return (
     <primitive
       object={fox.scene}
-      scale={1}
+      scale={.02}
       position={[0, -1, 0]}
       rotation-y={0.3}
     />
