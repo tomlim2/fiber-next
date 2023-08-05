6ea86ae75ffb7ea0c2b2e203aaad889084c96806
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-08-05 15:22:17 +0900

    Feat: Add shader to standard material

--- components/sections/fiber/animating-tri4/Experience.tsx
+++ components/sections/fiber/animating-tri4/Experience.tsx
@@ -25,18 +25,69 @@ import {
   MeshPhongMaterial,
   MeshPhysicalMaterial,
 } from "three";
+import CustomShaderMaterial from "three-custom-shader-material";
+import CustomShaderMaterialType from "three-custom-shader-material/vanilla";
 import { useControls } from "leva";
 import { shaders } from "./shader";
 
 const Experience = () => {
   const meshRef = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
   const { nodes } = useGLTF("/assets/models/aobox-transformed.glb") as any;
-  const materialRef = useRef<any>(null);
+  const materialRef = useRef<CustomShaderMaterialType>(null);
   const planeDimention = { width: 3, height: 3 };
   const sphereDimention = [1, 32, 32];
 
+  const intiValue = {
+    timeSpeedCtrl: 1,
+    paramsACtrl: 0.05,
+    colorACtrl: "#020202",
+    colorBCtrl: "#fffce9",
+  };
+
+  const timeSpeed = useRef(intiValue.timeSpeedCtrl);
+
+  const [_, set] = useControls(() => ({
+    colorACtrl: {
+      value: intiValue.colorACtrl,
+      onChange: (value) => {
+        if (materialRef && materialRef.current) {
+          materialRef.current.uniforms.uColorA.value = new Color(value);
+        }
+      },
+    },
+    colorBCtrl: {
+      value: intiValue.colorBCtrl,
+      onChange: (value) => {
+        if (materialRef && materialRef.current) {
+          materialRef.current.uniforms.uColorB.value = new Color(value);
+        }
+      },
+    },
+    paramsACtrl: {
+      value: intiValue.paramsACtrl,
+      step: 0.01,
+      min: -1,
+      max: 1,
+      onChange: (value) => {
+        if (materialRef && materialRef.current) {
+          materialRef.current.uniforms.uParamsA.value = value;
+        }
+      },
+    },
+    timeSpeedCtrl: {
+      value: intiValue.timeSpeedCtrl,
+      step: 0.1,
+      min: 1,
+      max: 10,
+      onChange: (value) => {
+        timeSpeed.current = value;
+      },
+    },
+  }));
+
   useEffect(() => {
-    // console.log(materialRef?.current);
+    set(intiValue);
+
     // Set the custom attribute on the mesh's geometry
   });
 
@@ -63,36 +114,13 @@ const Experience = () => {
   };
 
   useFrame((state) => {
-    // if (materialRef?.current) {
-    //   materialRef.current.uniforms.uTime.value = -state.clock.elapsedTime / 5;
-    // }
+    if (materialRef?.current) {
+      materialRef.current.uniforms.uTime.value = -state.clock.elapsedTime / 5;
+    }
   });
 
   const sphereGeometry = new SphereGeometry(1, 32, 32);
   const nonIndexedGeometry = sphereGeometry.toNonIndexed();
-
-  const updateMaterial = (shader: any) => {
-    shader.vertexShader = shader.vertexShader.replace(
-      "#include <common>",
-      `
-        #include <common>
-        uniform float uTime;
-        attribute float uARandom;
-        mat2 get2dRotateMatrix(float _angle)
-        {
-            return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
-        }
-      `
-    );
-    shader.vertexShader = shader.vertexShader.replace(
-      "#include <begin_vertex>",
-      `
-        #include <begin_vertex>
-
-        transformed += uARandom * normal;
-      `
-    );
-  };
 
   return (
     <>
@@ -117,9 +145,26 @@ const Experience = () => {
               onUpdateGeo(geometry);
             }}
           />
-          <meshStandardMaterial
+          <CustomShaderMaterial
+            baseMaterial={MeshStandardMaterial}
             ref={materialRef}
-            onBeforeCompile={(shader) => updateMaterial(shader)}
+            color={0x68c3c0}
+            roughness={0.2}
+            metalness={0.1}
+            flatShading={true}
+            // fragmentShader={shaders[0].fragment}
+            vertexShader={shaders[0].vertex}
+            uniforms={{
+              uTime: { value: 0 },
+              uWidth: { value: planeDimention.width },
+              uHeight: { value: planeDimention.height },
+              uMouseX: { value: 0 },
+              uMouseY: { value: 0 },
+              uParamsA: { value: 12 },
+              uColorA: { value: new Color("#1d79a0") },
+              uColorB: { value: new Color("#ffffff") },
+            }}
+            
           />
         </mesh>
       </group>
