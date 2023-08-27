3f7e5aca8d76a71b3284a11850a084ba7618d2e3
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-08-27 17:01:52 +0900

    Feat: Add ball on player tsxx

--- components/sections/fiber/journey-game2/Player.tsx
+++ components/sections/fiber/journey-game2/Player.tsx
@@ -1,5 +1,14 @@
-export default function Player()
-{
-    return <>
+import { RigidBody } from "@react-three/rapier";
+
+export default function Player() {
+  return (
+    <>
+      <RigidBody  canSleep={ false } colliders="ball" restitution={ 0.2 } friction={ 1 }  position={ [ 0, 1, 0 ] }>
+        <mesh castShadow>
+          <icosahedronGeometry args={[0.3, 1]} />
+          <meshStandardMaterial flatShading color="mediumpurple" />
+        </mesh>
+      </RigidBody>
     </>
+  );
 }
