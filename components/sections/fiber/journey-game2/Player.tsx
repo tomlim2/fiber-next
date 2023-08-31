52158522040e3887afcf4e3285f943b7b8c32d8e
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-08-31 20:52:53 +0900

    Feat: Add key board events

--- components/sections/fiber/journey-game2/Player.tsx
+++ components/sections/fiber/journey-game2/Player.tsx
@@ -1,16 +1,48 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 export default function Player() {
   const [subscribeKeys, getKeys] = useKeyboardControls();
-  useFrame(() => {
-    const keys = getKeys();
-    console.log(keys);
+  const body = useRef() as any;
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 0.6 * delta;
+    const torqueStrength = 0.2 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    }
+
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
   });
   return (
     <>
       <RigidBody
+        ref={body}
         canSleep={false}
         colliders="ball"
         restitution={0.2}
