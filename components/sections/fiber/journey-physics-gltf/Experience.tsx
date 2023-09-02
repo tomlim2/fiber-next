4aed201e9baea77ccc984ef1af8afc668ada9423
임연수(Younsoo Tom Lim) <tomandlim@gmail.com>
2023-09-02 18:20:21 +0900

    Fix: Mesh instances in journey

--- components/sections/fiber/journey-physics-gltf/Experience.tsx
+++ components/sections/fiber/journey-physics-gltf/Experience.tsx
@@ -1,15 +1,16 @@
 // "use client";
 import { useGLTF, Stage, OrbitControls } from "@react-three/drei";
 import {
+  InstancedRigidBodies,
+  CylinderCollider,
+  BallCollider,
+  CuboidCollider,
   RigidBody,
   Physics,
-  CuboidCollider,
-  CylinderCollider,
-  InstancedRigidBodies,
 } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
 import { useMemo, useEffect, useState, useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { Vector3, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const Experience = () => {
@@ -17,49 +18,32 @@ const Experience = () => {
   const hamburger = useGLTF("/assets/models/hamburger.glb");
   const cube = useRef(null) as any;
   const twister = useRef(null) as any;
-  const cubesCount = 30;
   const cubes = useRef(null) as any;
-  const cubeTransforms = useMemo(() => {
-    const positions: any[] = [];
-    const rotations: any[] = [];
-    const scales: any[] = [];
-    for (let i = 0; i < cubesCount; i++) {
-      positions.push([
-        (Math.random() - 0.5) * 8,
-        6 + i * 0.2,
-        (Math.random() - 0.5) * 8,
-      ]);
-      rotations.push([Math.random(), Math.random(), Math.random()]);
-
-      const scale = 0.2 + Math.random() * 0.8;
-      scales.push([scale, scale, scale]);
-    }
-
-    return { positions, rotations, scales };
-  }, []);
-
+  const cubesCount = 200;
   const collisionEnter = () => {
-    console.log("collision!");
     hitSound.currentTime = 0;
     hitSound.volume = Math.random();
     hitSound.play();
   };
+  const instances = useMemo(() => {
+    const instances = [] as any;
 
-  // useEffect(() => {
-  //   for (let i = 0; i < cubesCount; i++) {
-  //     const matrix = new THREE.Matrix4()
-  //       matrix.compose(
-  //           new THREE.Vector3(i * 2, 0, 0),
-  //           new THREE.Quaternion(),
-  //           new THREE.Vector3(1, 1, 1)
-  //       )
-  //       cubes.current.setMatrixAt(i, matrix)
-  //   }
-  // }, []);
+    for (let i = 0; i < cubesCount; i++) {
+      instances.push({
+        key: "instance_" + i,
+        position: [
+          (Math.random() - 0.5) * 8,
+          6 + i * 0.2,
+          (Math.random() - 0.5) * 8,
+        ],
+        rotation: [Math.random(), Math.random(), Math.random()],
+      });
+    }
+    return instances;
+  }, []);
 
   useFrame((state: any) => {
     const time = state.clock.getElapsedTime();
-    console.log(time);
     const eulerRotation = new THREE.Euler(0, time * 3, 0);
     const quaternionRotation = new THREE.Quaternion();
     quaternionRotation.setFromEuler(eulerRotation);
@@ -69,6 +53,7 @@ const Experience = () => {
     const z = Math.sin(angle);
     twister.current.setNextKinematicTranslation({ x: x, y: -0.8, z: z });
   });
+
   const cubeJump = () => {
     const mass = cube.current.mass();
     console.log("jump");
@@ -88,32 +73,20 @@ const Experience = () => {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
       <Physics gravity={[0, -9.81, 0]}>
-        <RigidBody colliders={false} position={[0, 4, 0]}>
+        <InstancedRigidBodies instances={instances}>
+          <instancedMesh
+            castShadow
+            receiveShadow
+            args={[undefined, undefined, cubesCount]}
+          >
+            <boxGeometry />
+            <meshStandardMaterial color="tomato" />
+          </instancedMesh>
+        </InstancedRigidBodies>
+        {/* <RigidBody colliders={false} position={[0, 4, 0]}>
           <primitive object={hamburger.scene} scale={0.25} />
           <CylinderCollider args={[0.65, 1.25]} position={[0, 0.7, 0]} />
         </RigidBody>
-        {/* <mesh castShadow position={[-2, 2, 0]}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-        </mesh> */}
-
-        {/* <RigidBody colliders="trimesh">
-          <mesh
-            castShadow
-            position={[0, 1, -0.25]}
-            rotation={[Math.PI * 0.1, 0, 0]}
-          >
-            <torusGeometry args={[1, 0.5, 16, 32]} />
-            <meshStandardMaterial color="mediumpurple" />
-          </mesh>
-        </RigidBody>
-
-        <RigidBody colliders="ball">
-          <mesh castShadow position={[0, 4, 0]}>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-          </mesh>
-        </RigidBody> */}
         <RigidBody colliders="ball" position={[-1.5, 2, 0]}>
           <mesh castShadow>
             <sphereGeometry />
@@ -145,6 +118,7 @@ const Experience = () => {
           </mesh>
           <CuboidCollider mass={2} args={[0.5, 0.5, 0.5]} />
         </RigidBody>
+        */}
         <RigidBody
           ref={twister}
           position={[0, -0.8, 0]}
